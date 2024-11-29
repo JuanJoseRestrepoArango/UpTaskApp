a8eee8b0b751bf05a05d27234e0614dad05429cf
JuanJoseRestrepoArango <restrepoj811@gmail.com>
2024-11-29 17:25:36 +0100

    FIrstsROutes

--- public/index.php
+++ public/index.php
@@ -2,9 +2,27 @@
 
 require_once __DIR__ . '/../includes/app.php';
 
+use Controllers\LoginController;
 use MVC\Router;
 $router = new Router();
 
+//RUTas login
+$router->get('/',[LoginController::class,'login']);
+$router->post('/',[LoginController::class,'login']);
+$router->get('/logout',[LoginController::class,'logout']);
+
+//Rutas Creacion de cuentas 
+$router->get('/crear',[LoginController::class,'crear']);
+$router->post('/crear',[LoginController::class,'crear']);
+//Rutas REcuperar de cuentas 
+$router->get('/recuperar',[LoginController::class,'recuperar']);
+$router->post('/recuperar',[LoginController::class,'recuperar']);
+//Rutas reestablecer de cuentas 
+$router->get('/reestablecer',[LoginController::class,'reestablecer']);
+$router->post('/reestablecer',[LoginController::class,'reestablecer']);
+//Rutas Confirmacion de cuentas 
+$router->get('/mensaje',[LoginController::class,'mensaje']);
+$router->get('/confirmar',[LoginController::class,'confirmar']);
 
 
 
