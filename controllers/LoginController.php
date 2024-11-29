85aeb7481e76683a144fcf9b98d3a3ba9429acd8
JuanJoseRestrepoArango <restrepoj811@gmail.com>
2024-11-29 18:25:26 +0100

    LoginStyles

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -1,13 +1,19 @@
 <?php
 namespace Controllers;
 
+use MVC\Router;
+
 class LoginController{
-    public static function login(){
-        echo "Desde Login";
+    public static function login(Router $router){
 
         if ($_SERVER['REQUEST_METHOD'] === 'POST'){
 
         }
+
+        //REnder a la vista
+        $router->render('auth/login',[
+            'titulo'=>'Iniciar Sesión'
+        ]);
     }
 
 
@@ -16,12 +22,16 @@ class LoginController{
 
     }
 
-    public static function crear(){
-        echo "Desde crear";
+    public static function crear(Router $router){
+
 
         if ($_SERVER['REQUEST_METHOD'] === 'POST'){
 
         }
+        //REnder a la vista
+        $router->render('auth/crear',[
+            'titulo'=>'Crear Cuenta'
+        ]);
     }
 
     public static function recuperar(){
