fe26d7bfe06977d7825537958c0b1bc993ff445b
JuanJoseRestrepoArango <restrepoj811@gmail.com>
2024-12-13 21:20:25 +0100

    Filters

--- src/js/tareas.js
+++ src/js/tareas.js
@@ -2,11 +2,28 @@
 
     obtenerTareas();
     let tareas = [];
+    let filtradas = [];
     // Boton para mostrar el modal para agregar tarea
     const nuevaTareaBtn = document.querySelector('#agregar-tarea');
     nuevaTareaBtn.addEventListener('click',function(){
         mostrarFormulario(false);
     });
+
+    //Filtros de Busqueda
+    const filtros = document.querySelectorAll('#filtros input[type="radio"]')
+    filtros.forEach(radio=>{
+        radio.addEventListener('input',filtrarTareas);
+    })
+
+    function filtrarTareas(e){
+        const filtro = e.target.value;
+        if(filtro !== ''){
+            filtradas = tareas.filter(tarea => tarea.estado === filtro)
+        }else{
+            filtradas = [];
+        }
+        mostrarTareas(filtradas)
+    }   
 
     async function obtenerTareas(){
 
@@ -26,7 +43,13 @@
         
         limpiarHTML();
 
-        if(tareas.length === 0){
+        totalPendientes();
+
+        totalCompletas();
+
+        const arrayTareas = filtradas.length ? filtradas : tareas;
+
+        if(arrayTareas.length === 0){
             const contenedorTareas = document.querySelector('#listado-tareas');
 
             const textoNoTareas = document.createElement('LI');
@@ -43,7 +66,7 @@
             1: 'Completa'
         };
 
-        tareas.forEach(tarea => {
+        arrayTareas.forEach(tarea => {
             const contenedorTarea = document.createElement('LI');
             contenedorTarea.dataset.tareaId = tarea.id;
             contenedorTarea.classList.add('tarea');
@@ -84,6 +107,29 @@
             const listadoTareas = document.querySelector('#listado-tareas');
             listadoTareas.appendChild(contenedorTarea);
         });
+    }
+
+    function totalPendientes(){
+        const totalPendientes = tareas.filter(tarea => tarea.estado =="0");
+        const pendientesRadio = document.querySelector('#pendientes');
+
+        if(totalPendientes.length === 0){
+            pendientesRadio.disabled = true;
+        }else{
+            pendientesRadio.disabled = false;
+        }
+
+    }
+    function totalCompletas(){
+        const totalCompletas = tareas.filter(tarea => tarea.estado =="1");
+        const completasRadio = document.querySelector('#completadas');
+
+        if(totalCompletas.length === 0){
+            completasRadio.disabled = true;
+        }else{
+            completasRadio.disabled = false;
+        }
+
     }
 
     function mostrarFormulario(editar = false,tarea = {}){
--- views/dashboard/proyecto.php
+++ views/dashboard/proyecto.php
@@ -8,6 +8,25 @@
             </button>
         </div>
     </div>
+
+    <div id="filtros" class="filtros">
+        <div class="filtros-inputs">
+            <h2>Filtros:</h2>
+            <div class="campos">
+                <label for="todas">Todas</label>
+                <input type="radio" name="filtro" id="todas" value="" checked>
+            </div>
+            <div class="campos">
+                <label for="completadas">Completadas</label>
+                <input type="radio" name="filtro" id="completadas" value="1">
+            </div>
+            <div class="campos">
+                <label for="pendientes">Pendientes</label>
+                <input type="radio" name="filtro" id="pendientes" value="0">
+            </div>
+        </div>
+    </div>
+
     <ul class="listado-tareas" id="listado-tareas">
 
     </ul>
